d7e443a1253f052303331d8b412eedd6227a6e8f
Anjitha Mohanan <113471696+AnjithaMohanan@users.noreply.github.com>
2024-02-12 10:41:12 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -26,17 +26,7 @@ const app = express();
 
 
 app.use(cors());
-const __dirname1=path.resolve();
-if(process.env.NODE_ENV==='production'){
-  app.use(express.static(path.join(__dirname1, "/client/build")))
-  app.get('*',(req,res)=>{
-    res.sendFile(path.resolve(__dirnname1,"client","build","index.html"))
-  })
-}else{
-  app.get('/', (req, res) => {
-    res.send('API RUNNING');
-  });
-}
+
 
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }))
